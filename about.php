5d0c6bdf67bcce28b6f9d2d52bcf5b11b9c3a79a
Chimaobi Okoye <okoyejnr2003@gmail.com>
2023-11-08 10:00:14 -0800

    Adding the about us page

---
+++ about.php
@@ -0,0 +1,13 @@
+<?php
+    // Creating the page title and adding the header page 
+    $pagetitle = "Login to your account";
+    require_once "assets/header.php";
+    
+    // Creating a connection to the database
+    require_once 'assets/db_connect.php';
+    
+?>
+
+<h1>
+    The first ecommerce store where weapons are sold globally.
+</h1>
