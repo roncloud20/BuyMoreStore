28a97f710a4888d6594cfe732313d990ebe790c1
Adeyina Gboluwaga <gboluwagaadeyina@gmail.com>
2023-11-08 11:02:22 -0800

    adding the sevice page

---
+++ service.php
@@ -0,0 +1,10 @@
+<?php
+$pagetitle = "Login to your account";
+require_once "assets/header.php";
+
+// Creating a connection to the database
+require_once 'assets/db_connect.php';
+
+?>
+<h1>Our Services</h1>
+<p>The services rendered by our organisation is superb and based on promoting self preference</p>
